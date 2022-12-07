8344ce4f8192e50cbff5c90e491a593a2296f6d3
ManoellyBernardo <manoellymaria@gmail.com>
2022-12-07 12:59:00 -0300

    Criação da página de cadastro de novos cargos.

---
+++ src/app/views/cargos/new-cargo/new-cargo.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Cargo } from 'src/app/models/cargo';
+import { CargoService } from 'src/app/services/cargo.service';
+
+@Component({
+  selector: 'app-new-cargo',
+  templateUrl: './new-cargo.component.html',
+  styleUrls: ['./new-cargo.component.css']
+})
+export class NewCargoComponent implements OnInit {
+
+  public formCargo: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cargoService: CargoService,
+    private router: Router
+  ) {
+    this.formCargo = formBuilder.group({
+      nome: ['', [Validators.required]],
+      descricao: ['', [Validators.required]],
+      salario: [0 , [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  public create(): void {
+    if (this.formCargo.valid) {
+      const cargo: Cargo = this.formCargo.value;
+      this.cargoService.create(cargo).subscribe(() => {
+        alert("Cargo cadastrado.");
+        this.router.navigate(["/cargos"]);
+      });
+    }
+    else {
+      alert("Dados inválidos.");
+    }
+  }
+
+}
