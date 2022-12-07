11a885698a16c25c15dd9768231cab4efc23e60a
ManoellyBernardo <manoellymaria@gmail.com>
2022-12-06 21:26:28 -0300

    Criação de página para visualizar a relação de cargos

---
+++ src/app/services/cargo.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { API_CONFIG } from '../config/api.config';
+import { Cargo } from '../models/cargo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CargoService {
+
+  constructor(private http: HttpClient) { }
+
+  public findAll(): Observable<Cargo[]> {
+    return this.http.get<Cargo[]>(`${API_CONFIG.baseUrl}/cargos`).pipe(
+      catchError(error => {
+        alert("Erro ao buscar dados de cargos.");
+        console.error(error);
+        return EMPTY;
+      })
+    );
+  } 
+}
